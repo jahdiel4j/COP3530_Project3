bbdc979d05b7718ff54cc35d1b0d66efddb53d38
Maggie Tieu <59667454+maggietieu@users.noreply.github.com>
2020-12-06 02:12:03 -0500

    Added more companies, calendars, search bar, etc.

--- GUI.py
+++ GUI.py
@@ -1,10 +1,17 @@
 import tkinter as tk
+#import tkinter.ttk as ttk
 import tkcalendar
+
+companies = ["ACN", "AT&T", "Adobe", "Amazon", "Appen", "Apple", "Bank of America", "Cisco", "Dell", "Disney", "Exxon", "Facebook",
+            "General Electric", "Google", "Hewlett-Packard", "IBM", "Intel", "JPMorgan Chase", "Kroger", "Lockheed Martin", "Microsoft",
+            "Netflix", "Oracle", "Proctor and Gamble", "Qualcomm", "Raytheon", "Salesforce", "UPS", "Wells Fargo"]
 
 """ --- Window Set-up --- """
 
 root = tk.Tk()
 root.title("Tech Stock Performance Analyzer")
+root.resizable(False, False)
+#ttk.Style('alt')
 
 # Dimensions of tk root window
 width = 1100
@@ -22,72 +29,234 @@ root.geometry('%dx%d+%d+%d' % (width, height, x, y))
 
 """ --- Drop-down Menu for Implementation Choice --- """
 
-structures = ["Hash Map", "AVL Tree"]
-clicked = tk.StringVar()
-clicked.set(structures[0])
+implementations = ["Hash Map", "AVL Tree"]
+imp_options = tk.StringVar()
+imp_options.set(implementations[0])
 
-menu = tk.OptionMenu(root, clicked, *structures)
-menu.place(relx=0.88, rely=0.04)
+implementation_menu = tk.OptionMenu(root, imp_options, *implementations)
+implementation_menu.config(bg="light steel blue", relief="groove")
+implementation_menu.place(relx=0.88, rely=0.03)
 
 
 """ --- Calendars --- """
 
 def retrieve_date():
-    test_date_retrieval.config(text=start_cal.get_date())
+    print(start_cal.get_date())
 
-start_cal_label = tk.Label(root, text="Start Date")
+start_cal_label = tk.Label(root, text="START DATE")
 start_cal_label.config(font=("Calibri", 12, "bold"))
-start_cal_label.place(relx=0.48, rely=0.1)
+start_cal_label.place(relx=0.493, rely=0.1)
 
-end_cal_label = tk.Label(root, text="End Date")
+end_cal_label = tk.Label(root, text="END DATE")
 end_cal_label.config(font=("Calibri", 12, "bold"))
-end_cal_label.place(relx=0.782, rely=0.1)
+end_cal_label.place(relx=0.7772, rely=0.1)
 
 start_cal = tkcalendar.Calendar(root, selectmode="day", year=2005, month=1, day=3, locale='en_US', date_pattern='MM/dd/yyyy')
-start_cal.place(relx=0.4, rely=0.16)
+start_cal.place(relx=0.422, rely=0.16)
 
 end_cal = tkcalendar.Calendar(root, selectmode="day", year=2020, month=11, day=27, locale='en_US', date_pattern='MM/dd/yyyy')
 end_cal.place(relx=0.7, rely=0.16)
 
-test_date_button = tk.Button(root, text="Get Date", command=retrieve_date)
-test_date_button.pack(pady=15)
 
-test_date_retrieval = tk.Label(root, text="")
-test_date_retrieval.pack(pady=15)
+""" --- Search Box --- """
+
+search_border = tk.Label(root, text="", borderwidth="1", relief="solid", width=66, height = 3)
+search_border.place(relx=0.468, rely=0.6)
+
+search_options = tk.StringVar()
+search_options.set(companies[0])
+
+search_menu = tk.OptionMenu(root, search_options, *companies)
+search_menu.config(relief="groove")
+search_menu.place(relx=0.485, rely=0.614)
+
+search_cal = tkcalendar.DateEntry(locale='en_US', date_pattern='MM/dd/yyyy')
+search_cal.place(relx=0.632, rely=0.619)
+
+search_button = tk.Button(root, text="Search")
+search_button.config(width=10, bg="light steel blue", relief="groove")
+search_button.place(relx=0.8, rely=0.617)
 
 
-""" --- Unfinished --- """
+""" --- Output --- """
+
+output_box_label = tk.Label(root, text="OUTPUT")
+output_box_label.config(font=("Calibri", 12, "bold"))
+output_box_label.place(relx=0.645, rely=0.708)
+
+output_box = tk.Text(root, width=68, height = 5)
+output_box.place(relx=0.425, rely=0.76)
+
+
+""" --- Companies --- """
+
+# Company options
+company_label = tk.Label(root, text="COMPANIES")
+company_label.config(font=("Calibri", 12, "bold"))
+company_label.place(relx=0.15, rely=0.1)
+
+# Column 1
+var1 = tk.StringVar()
+google = tk.Checkbutton(root, text="Google", variable=var1, onvalue = "Google is checked", offvalue="Google is unchecked")
+google.deselect()
+google.config(font=("Calibri", 12))
+google.place(relx=0.07, rely=0.16)
+
+microsoft = tk.Checkbutton(root, text="Microsoft")
+microsoft.deselect()
+microsoft.config(font=("Calibri", 12))
+microsoft.place(relx=0.07, rely=0.21)
+
+acn = tk.Checkbutton(root, text="ACN")
+acn.deselect()
+acn.config(font=("Calibri", 12))
+acn.place(relx=0.07, rely=0.26)
+
+adobe = tk.Checkbutton(root, text="Adobe")
+adobe.deselect()
+adobe.config(font=("Calibri", 12))
+adobe.place(relx=0.07, rely=0.31)
+amazon = tk.Checkbutton(root, text="Amazon")
+amazon.deselect()
+amazon.config(font=("Calibri", 12))
+amazon.place(relx=0.07, rely=0.36)
+
+cisco = tk.Checkbutton(root, text="Cisco")
+cisco.deselect()
+cisco.config(font=("Calibri", 12))
+cisco.place(relx=0.07, rely=0.41)
+
+fb = tk.Checkbutton(root, text="Facebook")
+fb.deselect()
+fb.config(font=("Calibri", 12))
+fb.place(relx=0.07, rely=0.46)
+
+ibm = tk.Checkbutton(root, text="IBM")
+ibm.deselect()
+ibm.config(font=("Calibri", 12))
+ibm.place(relx=0.07, rely=0.51)
+
+intel = tk.Checkbutton(root, text="Intel")
+intel.deselect()
+intel.config(font=("Calibri", 12))
+intel.place(relx=0.07, rely=0.56)
+
+netflix = tk.Checkbutton(root, text="Netflix")
+netflix.deselect()
+netflix.config(font=("Calibri", 12))
+netflix.place(relx=0.07, rely=0.61)
+
+oracle = tk.Checkbutton(root, text="Oracle")
+oracle.deselect()
+oracle.config(font=("Calibri", 12))
+oracle.place(relx=0.07, rely=0.66)
+
+salesforce = tk.Checkbutton(root, text="Salesforce")
+salesforce.deselect()
+salesforce.config(font=("Calibri", 12))
+salesforce.place(relx=0.07, rely=0.71)
+
+qualcom = tk.Checkbutton(root, text="Qualcomm")
+qualcom.deselect()
+qualcom.config(font=("Calibri", 12))
+qualcom.place(relx=0.07, rely=0.76)
+
+exxon = tk.Checkbutton(root, text="Exxon")
+exxon.deselect()
+exxon.config(font=("Calibri", 12))
+exxon.place(relx=0.07, rely=0.81)
+
+att = tk.Checkbutton(root, text="AT&T")
+att.deselect()
+att.config(font=("Calibri", 12))
+att.place(relx=0.07, rely=0.86)
+
+# Column 2
+apple = tk.Checkbutton(root, text="Apple")
+apple.deselect()
+apple.config(font=("Calibri", 12))
+apple.place(relx=0.22, rely=0.16)
+
+bankA = tk.Checkbutton(root, text="Bank of America")
+bankA.deselect()
+bankA.config(font=("Calibri", 12))
+bankA.place(relx=0.22, rely=0.21)
+
+ge = tk.Checkbutton(root, text="General Electric")
+ge.deselect()
+ge.config(font=("Calibri", 12))
+ge.place(relx=0.22, rely=0.26)
+
+lock = tk.Checkbutton(root, text="Lockhead Martin")
+lock.deselect()
+lock.config(font=("Calibri", 12))
+lock.place(relx=0.22, rely=0.31)
+
+hp = tk.Checkbutton(root, text="Hewlett-Packard")
+hp.deselect()
+hp.config(font=("Calibri", 12))
+hp.place(relx=0.22, rely=0.36)
+
+jpm = tk.Checkbutton(root, text="JPMorgan Chase")
+jpm.deselect()
+jpm.config(font=("Calibri", 12))
+jpm.place(relx=0.22, rely=0.41)
+
+pg = tk.Checkbutton(root, text="Proctor and Gamble")
+pg.deselect()
+pg.config(font=("Calibri", 12))
+pg.place(relx=0.22, rely=0.46)
+
+ups = tk.Checkbutton(root, text="UPS")
+ups.deselect()
+ups.config(font=("Calibri", 12))
+ups.place(relx=0.22, rely=0.51)
+
+dell = tk.Checkbutton(root, text="Dell")
+dell.deselect()
+dell.config(font=("Calibri", 12))
+dell.place(relx=0.22, rely=0.56)
+
+raytheon = tk.Checkbutton(root, text="Raytheon")
+raytheon.deselect()
+raytheon.config(font=("Calibri", 12))
+raytheon.place(relx=0.22, rely=0.61)
+
+wf = tk.Checkbutton(root, text="Wells Fargo")
+wf.deselect()
+wf.config(font=("Calibri", 12))
+wf.place(relx=0.22, rely=0.66)
+
+appen = tk.Checkbutton(root, text="Appen")
+appen.deselect()
+appen.config(font=("Calibri", 12))
+appen.place(relx=0.22, rely=0.71)
+
+disney = tk.Checkbutton(root, text="Disney")
+disney.deselect()
+disney.config(font=("Calibri", 12))
+disney.place(relx=0.22, rely=0.76)
+
+kroger = tk.Checkbutton(root, text="Kroger")
+kroger.deselect()
+kroger.config(font=("Calibri", 12))
+kroger.place(relx=0.22, rely=0.81)
+
+
+""" --- Analysis Buttons --- """
 
 # Displays text depending on the boxes checked
 def checked():
     l = tk.Label(root, text=var1.get())
     l.pack()
 
-# Company options
-company_label = tk.Label(root, text="COMPANIES")
-company_label.config(font=("Calibri", 12, "bold"))
-company_label.place(relx=0.16, rely=0.1)
+best_button = tk.Button(root, text="Find Best Stock Growth", command=[checked(), retrieve_date()])
+best_button.config(width=20, bg="light steel blue", relief="groove")
+best_button.place(relx=0.607, rely=0.46)
 
-var1 = tk.StringVar()
-google = tk.Checkbutton(root, text="Google", variable=var1, onvalue = "Google is checked", offvalue="Google is unchecked")
-google.deselect()
-google.config(font=("Calibri", 12))
-google.place(relx=0.08, rely=0.16)
-
-microsoft = tk.Checkbutton(root, text="Microsoft")
-microsoft.deselect()
-microsoft.config(font=("Calibri", 12))
-microsoft.place(relx=0.08, rely=0.21)
-
-apple = tk.Checkbutton(root, text="Apple")
-apple.deselect()
-apple.config(font=("Calibri", 12))
-apple.place(relx=0.25, rely=0.16)
-
-# TO-DO: Add rest of checkbox companies
-
-b = tk.Button(root, text="Run Analysis", command=checked)
-b.place(relx=0.45, rely=0.9)
+worst_button = tk.Button(root, text="Find Worst Stock Growth", command=[checked(), retrieve_date()])
+worst_button.config(width =20, bg="light steel blue", relief="groove")
+worst_button.place(relx=0.607, rely=0.52)
 
 
 root.mainloop()
